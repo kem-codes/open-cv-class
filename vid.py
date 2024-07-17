ebc404d6bc1b635b08890332edda8fc080f69e18
kemi <150168585+kem-codes@users.noreply.github.com>
2024-07-17 14:07:00 +0100

    Changed Title

--- vid.py
+++ vid.py
@@ -9,7 +9,7 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x,y), (x+w,  y+h),
         (0, 255,0),5)
-        cv2.imshow("Video", image)
+        cv2.imshow("Faces Video", image)
         k=cv2.waitKey(30) & 0xFF
         if k ==27:
             break
